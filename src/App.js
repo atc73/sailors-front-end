b08612630eaad3a6d66f8f599ef926551f8283d8
Joris <joris.sellam@gmail.com>
2022-08-31 13:19:54 +0200

    added comments

--- src/App.js
+++ src/App.js
@@ -1,76 +1,89 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import styles from './App.module.scss';
 import SailorForm from './sailorForm';
 import SailorList from './sailorList';
 
 const App = () => {
+  // état qui contient la liste des marins enregistrés
+  const [sailorList, setSailorList] = useState([]);
 
-  const [sailorList, setSailorList] = useState([])
+  // état pour le prénom et le nom du formulaire
   const [sailorToAdd, setSailorToAdd] = useState({
     firstName: '',
-    lastName: ''
+    lastName: '',
   });
 
+  // Fonction asynchrone qui obtient la liste des tous les marins
   const fetchData = async () => {
     try {
-        let response = await fetch(process.env.REACT_APP_BACK_END);
-        let data = await response.json();
-        setSailorList(data);
+      let response = await fetch(process.env.REACT_APP_BACK_END);
+      let data = await response.json();
+      setSailorList(data);
     } catch (error) {
-        alert(error)
+      alert(error);
     }
-  }
+  };
 
-const postData = async () => {
-  try {
-    await fetch(process.env.REACT_APP_BACK_END, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(sailorToAdd),
+  // Fonction asynchrone qui ajoute un marin
+  const postData = async () => {
+    try {
+      await fetch(process.env.REACT_APP_BACK_END, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(sailorToAdd),
+      });
+      fetchData();
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  // Fonction asynchrone qui supprime un marin en fonction de son id
+  const deleteData = async (id) => {
+    try {
+      await fetch(process.env.REACT_APP_BACK_END + `/${id}`, {
+        method: 'delete',
+      });
+      fetchData();
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // Déclenche la fonction qui ajoute un marin et réinitialise l'état du marin à ajouter
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postData();
+    setSailorToAdd({
+      firstName: '',
+      lastName: '',
     });
-    fetchData()
-  } catch (error) {
-    alert(error)
-  }
-}
+  };
 
-const deleteData = async (id) => {
-  try {
-    await fetch(process.env.REACT_APP_BACK_END +`/${id}`, {
-      method: "delete"
-    });
-    fetchData()
-  } catch (error) {
-    alert(error)
-  }
-}
-  
-useEffect(() => {
-  fetchData();
-}, [])
-
-const handleSubmit = (e) => {
-  e.preventDefault()
-  postData()
-  setSailorToAdd({
-    firstName: '',
-    lastName: ''
-  })
-}
-
-const handleDelete = (itemId) => {
-  deleteData(itemId)
-}
-
+  // Déclenche la fonction qui supprime un marin
+  const handleDelete = (itemId) => {
+    deleteData(itemId);
+  };
 
   return (
     <div className={styles.appContainer}>
-      <SailorForm sailorToAdd={sailorToAdd} setSailorToAdd={setSailorToAdd} handleSubmit={handleSubmit} />
+      {/* Formulaire pour ajouter un marin */}
+      <SailorForm
+        sailorToAdd={sailorToAdd}
+        setSailorToAdd={setSailorToAdd}
+        handleSubmit={handleSubmit}
+      />
+
+      {/* Liste de tous les marins enregistrés */}
       <SailorList sailorList={sailorList} handleDelete={handleDelete} />
     </div>
   );
-}
+};
 
 export default App;
